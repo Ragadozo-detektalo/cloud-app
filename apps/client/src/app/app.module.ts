19028e214d146d1f9dd3a71c5161c896943bc5e2
dosabalint <dosa.balint@gmail.com>
2021-04-30 20:24:10 +0200

    client init

--- apps/client/src/app/app.module.ts
+++ apps/client/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, HttpClientModule],
+  imports: [BrowserModule, HttpClientModule, BrowserAnimationsModule, MatTreeModule, MatToolbarModule, MatIconModule, MatButtonModule, MatTableModule],
   providers: [],
   bootstrap: [AppComponent],
 })
